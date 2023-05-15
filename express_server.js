c09448240631ac9f575de705aa38f64145bc33c3
Tamara Tell <2telltamara@gmail.com>
2023-05-15 15:24:45 -0400

    setup ejs and create views directory

--- express_server.js
+++ express_server.js
@@ -3,6 +3,8 @@ const res = require('express/lib/response');
 const app = express();
 const PORT = 8080;
 
+app.set("view engine", "ejs");
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -10,6 +12,10 @@ const urlDatabase = {
 
 app.get('/', (req, res) => {
   res.send("Welcome to my homepage!");
+});
+
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
 });
 
 app.listen(PORT, () => {
